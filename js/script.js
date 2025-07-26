d0bf123c12c6a6d467ae8994598a4e9fe74c9959
Victore Bouvie <victore.bouvie.dev@gmail.com>
2025-07-25 22:38:05 -0300

    feat: Implement JavaScript logic

---
+++ js/script.js
@@ -0,0 +1,45 @@
+const apiUrl = 'http://127.0.0.1:5000/api/projects'
+
+async function fetchAndDisplayProjects() {
+    try{
+        const response = await fetch(apiUrl)
+        const projects = await response.json()
+        const container = document.getElementById('projects-container')
+        container.innerHTML = ''
+
+        projects.forEach(project =>{
+        const projectCard = document.createElement('article')
+        projectCard.className = 'project-card'
+
+        projectCard.innerHTML = `
+        <h3>${project.name}</h3>
+        <p>${project.description}</p>
+        <div class="technologies">
+            ${project.technologies.map(tech => `<span>${tech}</span>`).join('')}
+        </div>
+        <div class="project-links">
+            <a href="${project.github_url}" target="_blank">See in GitHub</a>
+        </div>
+        `
+        container.appendChild(projectCard)
+    })
+    } catch (error){
+        console.error("Error trying to search projects")
+        const container = document.getElementById('projects-container')
+        container.innerHTML = '<p>It was not possible load the projects, try again later.</p>'
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerBtn = document.getElementById('hamburger-btn')
+    const mainNav = document.querySelector('.main-nav')
+
+    if (hamburgerBtn && mainNav) {
+        hamburgerBtn.addEventListener('click', () => {
+            mainNav.classList.toggle('is-active')
+            hamburgerBtn.classList.toggle('is-active')
+        })
+    }
+})
+
+fetchAndDisplayProjects()
