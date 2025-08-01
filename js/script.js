5e7c39e7496b1e19aaa7200d322982c9876b6d5b
Victore Bouvie <victore.bouvie.dev@gmail.com>
2025-08-01 03:49:14 -0300

    fix: im tired. just removing old trashhhhhhhhh

--- js/script.js
+++ js/script.js
@@ -236,7 +236,6 @@ const App = {
 
   setupAnimations() {
     this.triggerInitialAnimation()
-    this.setupScrollObserver()
   },
 
   triggerInitialAnimation() {
@@ -247,25 +246,6 @@ const App = {
             el.classList.remove('hidden-on-load')
         }, 100)
     })
-  },
-
-  setupScrollObserver() {
-    const observerOptions = {
-        root: null,
-        rootMargin: '0px',
-        threshold: 0.1,
-    }
-    const observerCallback = (entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('visible')
-                observer.unobserve(entry.target)
-            }
-        })
-    }
-    const scrollObserver = new IntersectionObserver(observerCallback, observerOptions)
-    const elementsToAnimate = document.querySelectorAll('.hidden-on-scroll')
-    elementsToAnimate.forEach(el => scrollObserver.observe(el))
   },
 }
 
